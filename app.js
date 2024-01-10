ac5246fe3669bed66c1ea3f4cc9a0eb9bfc288dc
coderRahul212000 <rahulrajput211998@gmail.com>
2024-01-11 00:27:21 +0530

    Lecture 2

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const expressSession = require("express-session");
 const passport = require("passport")
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const flash = require("connect-flash")
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(flash());
 
 app.use(expressSession({
   resave:false,
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const userModel = require("./users")
 const postModel = require("./posts");
 const passport = require('passport');
 const localStrategy = require("passport-local")
+const upload = require("./multer")
 
 // is line sai user sign in hota hai
 passport.use(new localStrategy(userModel.authenticate()))
@@ -14,11 +15,24 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/login', function (req, res, next) {
-  res.render('login');
+  // console.log(req.flash("error"))
+
+  res.render('login', {error: req.flash("error")});
 });
 
-router.get('/profile',isLoggedIn, function (req, res, next) {
-  res.render("profile");
+router.get('/profile',isLoggedIn, async function (req, res, next) {
+  // const user = await userModel.findOne({
+  //   // req.session.passport.user mai login k baad hmara logged in user ajata hai
+  //   username : req.session.passport.user
+  // });
+
+  const user = await userModel.findOne({
+    // req.session.passport.user mai login k baad hmara logged in user ajata hai
+    username : req.session.passport.user
+  }).populate("posts");
+  console.log(user)
+  // console.log(user)
+  res.render("profile", {user});
 });
 
 router.post("/register", (req,res)=>{
@@ -35,7 +49,8 @@ router.post("/register", (req,res)=>{
 
 router.post("/login", passport.authenticate("local",{
   successRedirect : "/profile",
-  failureRedirect: "/login"
+  failureRedirect: "/login",
+  failureFlash:true //issai agar properly logged in ni ho payega toh flash messages dikh payenge
 }), (req,res)=>{
 
 })
@@ -44,7 +59,27 @@ router.get("/feed", (req,res,next)=>{
   res.render("feed")
 })
 
-router.get("/logout", (req,res)=>{
+router.post("/upload",isLoggedIn, upload.single("file") , async (req,res)=>{
+  if(!req.file){
+    return res.status(404).send("No files were uploaded")
+  }
+  // res.send("File uploaded successfully")
+  
+  //jo file upload hui hai oosai save karo as a post and uska postid user ko doo and post ko userid doo
+  const user = await userModel.findOne({username: req.session.passport.user})
+  const post = await postModel.create({
+    image: req.file.filename, // ismai hota  hai jo file upload hui hai uska name
+    imageText: req.body.filecaption,
+    user: user._id 
+  })
+
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile")
+
+})
+
+router.post("/logout", (req,res)=>{
   req.logOut((err)=>{
     if (err) {return next(err);}
     res.redirect("/");
